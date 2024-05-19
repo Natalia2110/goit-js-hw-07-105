692598b4bd1365a3ef673441915ab294793669cc
Nataliya Yakovyna <yakovina.n13@gmail.com>
2024-05-19 14:23:45 +0300

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const refs = {
+  bodyEl: document.querySelector('body'),
+  spanEl: document.querySelector('.color'),
+  btnEl: document.querySelector('.change-color'),
+}
+// console.log(refs.bodyEl);
+// console.log(refs.spanEl);
+// console.log(refs.btnEl);
+
+function handleBtnElClick(event) {
+  const color = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = color;
+  refs.spanEl.textContent = `${color}`;
+};
+
+refs.btnEl.addEventListener('click', handleBtnElClick);
