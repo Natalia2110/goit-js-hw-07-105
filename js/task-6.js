fefc9c88c593906a76c64f749b280a10fbe2c5e9
Nataliya Yakovyna <yakovina.n13@gmail.com>
2024-05-23 23:28:25 +0300

    task-06

--- js/task-6.js
+++ js/task-6.js
@@ -12,39 +12,46 @@ const refs = {
 };
 
 function createBoxes(amount) {
-
   // const arr = [];
+  let boxTemplate = "";
   for (let i = 0; i < amount; i++) {
-    // refs.divElem.innerHTML = '' 
-    const color = getRandomHexColor();
-    const boxTemplate = document.createElement('div');
-    boxTemplate.style.width = `${i * 10 + 30}px`;
-    boxTemplate.style.height = `${i * 10 + 30}px`;
-    boxTemplate.style.backgroundColor = `${color}`;
-    // arr.push(boxTemplate);
-    
-    refs.divElem.append(boxTemplate);
+    // refs.divElem.innerHTML = ''
+    // const color = getRandomHexColor();
+    // const boxTemplate = document.createElement("div");
+    // boxTemplate.style.width = `${i * 10 + 30}px`;
+    // boxTemplate.style.height = `${i * 10 + 30}px`;
+    // boxTemplate.style.backgroundColor = `${color}`;
+
+    // refs.divElem.append(boxTemplate);
+
+    boxTemplate += `<div style="width: ${i * 10 + 30}px; height: ${
+      i * 10 + 30
+    }px; background-color: ${getRandomHexColor()}"></div>`;
   }
+  return boxTemplate;
+  // refs.divElem.innerHTML = boxTemplate;
   // console.log(arr);
 }
 
 function handleBtnCreateElem() {
-  refs.divElem.innerHTML = ''
+  // refs.divElem.innerHTML = "";
   const inputValue = Number(refs.inputElem.value);
-  if (inputValue >= 1  && inputValue <= 100) {
-    createBoxes(inputValue);
-    refs.inputElem.value = '';
-  
-    // refs.divElem.insertAdjacentHTML('beforeend', markup);
+  if (inputValue >= 1 && inputValue <= 100) {
+    const markup = createBoxes(inputValue);
+    refs.inputElem.value = "";
+
+    refs.divElem.insertAdjacentHTML("beforeend", markup);
     // refs.divElem.innerHTML = markup;
-    }
+  } else {
+    alert("Please enter a number between 1 and 100");
+  }
 }
-  
+
 function destroyBoxes() {
-  refs.divElem.innerHTML = '';
+  refs.divElem.innerHTML = "";
   // const allBoxes = refs.divElem.querySelectorAll('div')
   // allBoxes.remove();
 }
 
-refs.btnCreateElem.addEventListener('click', handleBtnCreateElem);
-refs.btnDestroyElem.addEventListener('click', destroyBoxes);
+refs.btnCreateElem.addEventListener("click", handleBtnCreateElem);
+refs.btnDestroyElem.addEventListener("click", destroyBoxes);
